976cf55b38a2bca30f7babfc1e1007cfccf8b833
Hiếu Thuận Nguyễn <nguyenthuan2704@gmail.com>
2023-06-17 19:58:38 +0700

    testing deployment

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn AWS And Docker & K8S
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
